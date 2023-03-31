2eda3fced98dd4753193945816cdda3bf2a7a15e
Murat Barış Erol <58422636+bariserol761@users.noreply.github.com>
2023-03-31 17:57:11 +0300

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,14 @@ import { EventController } from './event/event.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { EmailController } from './email.controller';
 import { join } from 'path/posix';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { KeycloakModule } from '@nestjs/keycloak';
+
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://emre:123@cluster0.wqgmfoi.mongodb.net',
       { dbname: 'WinMeetDB' },
     ),
+
     MongooseModule.forFeature([{ name: 'Event', schema: EventSchema }]),
 
     MailerModule.forRoot({
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException, Redirect, Res } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose'
+import { InjectModel } from '@nestjs/mongoose';
 import { EventInterface } from 'src/interface/event.interface';
 import { CreateEventDto } from 'src/dto/create-event.dto';
 import { UpdateEventDto } from 'src/dto/update-event.dto';
@@ -9,69 +9,76 @@ import { EventSchema } from 'src/schema/event.schema';
 import { MailerService } from '@nestjs-modules/mailer';
 @Injectable()
 export class EventService {
-    constructor(@InjectModel('Event') private eventModel:Model<EventInterface>, private mailService: MailerService){}
-  
-    async sendMail(participants: string[], superHero: any) {
-      
-        for(var i=0;i<participants.length;i++) {
-        await this.mailService.sendMail({
-            to:participants[i],
-            from:"emredurmus06@hotmail.com",
-            subject: 'WinMeet Template',
-            template: 'superhero', 
-            context: {
-                 superHero:superHero   
-            },
-           });
-           //return response;
-        }
+  constructor(
+    @InjectModel('Event') private eventModel: Model<EventInterface>,
+    private mailService: MailerService,
+  ) {}
+
+  async sendMail(participants: string[], superHero: any) {
+    for (var i = 0; i < participants.length; i++) {
+      await this.mailService.sendMail({
+        to: participants[i],
+        from: 'emredurmus06@hotmail.com',
+        subject: 'WinMeet Template',
+        template: 'superhero',
+        context: {
+          superHero: superHero,
+        },
+      });
+      //return response;
     }
-    //creating event
-    async createEvent(createEventDto:CreateEventDto):Promise<EventInterface>{
-        const newEvent = await new this.eventModel(createEventDto)
-       /* createEventDto.eventStartDate = new Date(createEventDto.eventStartDate);
+  }
+  //creating event
+  async createEvent(createEventDto: CreateEventDto): Promise<EventInterface> {
+    const newEvent = await new this.eventModel(createEventDto);
+    /* createEventDto.eventStartDate = new Date(createEventDto.eventStartDate);
         createEventDto.eventEndDate = new Date(createEventDto.eventEndDate);*/
-        this.sendMail(createEventDto.participants, createEventDto);
-        return newEvent.save();
-        
-   
+    this.sendMail(createEventDto.participants, createEventDto);
+    return newEvent.save();
+  }
+
+  //read all events
+  async getAllEvents(): Promise<EventInterface[]> {
+    const eventData = await this.eventModel.find();
+    if (!eventData || eventData.length == 0) {
+      throw new NotFoundException('Event data not found');
+    }
+    return eventData;
+  }
+
+  //search event by id
+  async findByid(eventId: string): Promise<EventInterface> {
+    const existingEvent = await this.eventModel.findById(eventId);
+    if (!existingEvent) {
+      throw new NotFoundException('Event not found');
+    }
+    return existingEvent;
+  }
+
+  //delete event by id
+  async deleteEvent(eventId: string): Promise<EventInterface> {
+    const deletedEvent = await this.eventModel.findByIdAndDelete(eventId);
+    if (!deletedEvent) {
+      throw new NotFoundException('Event not found');
     }
 
-    //read all events
-    async getAllEvents():Promise<EventInterface[]>{
-        const eventData = await this.eventModel.find()
-        if(!eventData || eventData.length == 0){
-            throw new NotFoundException("Event data not found")
-        }
-        return eventData
+    return deletedEvent;
+  }
+
+  //update event by id
+  async updateEvent(
+    eventId: string,
+    updateData: UpdateEventDto,
+  ): Promise<EventInterface> {
+    const existingEvent = await this.eventModel.findByIdAndUpdate(
+      eventId,
+      updateData,
+      { new: true },
+    );
+    if (!existingEvent) {
+      throw new NotFoundException('Event not found');
     }
 
-    //search event by id
-    async findByid(eventId:string):Promise<EventInterface>{
-        const existingEvent = await this.eventModel.findById(eventId)
-        if(!existingEvent){
-            throw new NotFoundException("Event not found")
-        }
-        return existingEvent
-    }
-
-    //delete event by id
-    async deleteEvent(eventId:string):Promise<EventInterface>{
-        const deletedEvent = await this.eventModel.findByIdAndDelete(eventId)
-        if(!deletedEvent){
-            throw new NotFoundException("Event not found")
-        }
-
-        return deletedEvent
-    }
-
-    //update event by id
-    async updateEvent(eventId:string,updateData:UpdateEventDto):Promise<EventInterface>{
-        const existingEvent = await this.eventModel.findByIdAndUpdate(eventId,updateData,{new: true})
-        if(!existingEvent){
-            throw new NotFoundException("Event not found")
-        }
-
-        return existingEvent
-    }
+    return existingEvent;
+  }
 }
