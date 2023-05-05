78f603bdaaa41da7cb39b9af9ed5343685bc9795
Murat Barış Erol <58422636+bariserol761@users.noreply.github.com>
2023-05-05 16:56:26 +0300

    sada

--- src/interface/event.interface.ts
+++ src/interface/event.interface.ts
@@ -1,10 +1,17 @@
-import { Document } from 'mongoose';
+mport { Document } from 'mongoose';
 export interface EventInterface extends Document {
+
+  readonly eventOwner: string;
+
   readonly eventName: string;
 
   readonly eventDescription: string;
 
   readonly location: string;
+
+  readonly eventDates: Array<Date>;
+
+  readonly eventPollDueDate: Date;
 
   readonly eventStartDate: Date;
 
